467be865157263b9dbbe0bef86ef0ea0e952198a
footlessbird <>
2019-04-22 21:52:10 -0700

    DB migration

--- pyBlog/admin.py
+++ pyBlog/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .models import Post
 
-# Register your models here.
+# localhost:8000/admin에 Post 등록
+admin.site.register(Post)
--- pyBlog/views.py
+++ pyBlog/views.py
@@ -1,30 +1,14 @@
 from django.shortcuts import render
-
-posts = [
-    {
-        'author': 'Shazam',
-        'title': 'Shaaaaaaaazam!!!!!',
-        'content': 'power overwhelming',
-        'date_posted': 'April 22, 2019'
-
-    },
-    {
-        'author': 'Batman',
-        'title': 'The Dark Knight',
-        'content': 'I am not Bruce Wayne',
-        'date_posted': 'April 21, 2019'
-
-    },
-]
+from .models import Post
 
 
 # 'pyBlog/home.html' 처럼 pyBlog path정해주면 에러발생! 알아서 default로 templates디렉토리에 내에 있다고 설정되 있는 듯
 def home(request):
     context = {
-        'posts': posts
+        'posts': Post.objects.all()
     }
     return render(request, 'home.html', context)
 
 
 def about(request):
-    return render(request, 'about.html', {'title':'About'})
+    return render(request, 'about.html', {'title': 'About'})
