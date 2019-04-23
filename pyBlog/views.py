86541180787cd6e58bb2dfce05be755d1c3723f7
footlessbird <>
2019-04-22 18:06:37 -0700

    inherit parent html & bootstrap applied

--- pyBlog/views.py
+++ pyBlog/views.py
@@ -1,9 +1,30 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+
+posts = [
+    {
+        'author': 'Shazam',
+        'title': 'Shaaaaaaaazam!!!!!',
+        'content': 'power overwhelming',
+        'date_posted': 'April 22, 2019'
+
+    },
+    {
+        'author': 'Batman',
+        'title': 'The Dark Knight',
+        'content': 'I am not Bruce Wayne',
+        'date_posted': 'April 21, 2019'
+
+    },
+]
 
 
+# 'pyBlog/home.html' 처럼 pyBlog path정해주면 에러발생! 알아서 default로 templates디렉토리에 내에 있다고 설정되 있는 듯
 def home(request):
-    return HttpResponse('<h1>Home</h1>')
+    context = {
+        'posts': posts
+    }
+    return render(request, 'home.html', context)
+
 
 def about(request):
-    return HttpResponse('<h1>About</h1>')
+    return render(request, 'about.html', {'title':'About'})
